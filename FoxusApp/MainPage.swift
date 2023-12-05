5f5605045ea9925575243f9153739c1b038467b6
AlJawharhALOtaibi <ALJawharhALOtaibi@gmail.com>
2023-12-05 17:49:35 +0300

    Copy 2

---
+++ FoxusApp/MainPage.swift
@@ -0,0 +1,18 @@
+//
+//  MainPage.swift
+//  FoxusApp
+//
+//  Created by AlJawharh AlOtaibi on 21/05/1445 AH.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPage()
+}
---
+++ FoxusApp/tettete.swift
@@ -0,0 +1,18 @@
+//
+//  tettete.swift
+//  FoxusApp
+//
+//  Created by AlJawharh AlOtaibi on 21/05/1445 AH.
+//
+
+import SwiftUI
+
+struct tettete: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    tettete()
+}
